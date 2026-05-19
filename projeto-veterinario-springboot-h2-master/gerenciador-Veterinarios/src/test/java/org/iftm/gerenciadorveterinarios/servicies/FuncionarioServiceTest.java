0983eb082d6ba7976f7c39292b2d9f2efb2c168f
Arthur Santana Cavichioli <arthurcavic@gmail.com>
2026-05-18 23:50:15 -0300

    Ciclo 1: Regra de Negócio no Cadastro (Status Padrão)
    red ok

--- projeto-veterinario-springboot-h2-master/gerenciador-Veterinarios/src/test/java/org/iftm/gerenciadorveterinarios/servicies/FuncionarioServiceTest.java
+++ projeto-veterinario-springboot-h2-master/gerenciador-Veterinarios/src/test/java/org/iftm/gerenciadorveterinarios/servicies/FuncionarioServiceTest.java
@@ -1,6 +1,14 @@
 package org.iftm.gerenciadorveterinarios.servicies;
 
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+
+import org.h2.command.dml.MergeUsing;
+import org.iftm.gerenciadorveterinarios.entities.Funcionario;
 import org.iftm.gerenciadorveterinarios.repositories.FuncionarioRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -14,4 +22,15 @@ public class FuncionarioServiceTest {
 
     @InjectMocks
     private FuncionarioService service;
+
+    @Test
+    @DisplayName("Retorna funcionários que não estão de férias")
+    public void deveRetornarFuncionariosQueNaoEstaoDeFerias() {
+        //Arrange
+        Funcionario f1 = new Funcionario(1, "Ana", "Veterinario(a)", BigDecimal.valueOf(10000), false);
+        Funcionario f2 = new Funcionario(2, "Bruno", "Técnico em Veterinária", BigDecimal.valueOf(4000), false);
+        List<Funcionario> listaEsperada = Arrays.asList(f1, f2);
+
+        when(repositorio.findByEmFerias(false)).thenReturn(listaEsperada);
+    }
 }
