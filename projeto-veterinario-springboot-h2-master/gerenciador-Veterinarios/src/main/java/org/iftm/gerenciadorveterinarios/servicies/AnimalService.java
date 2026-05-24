36cee1f49441a3d884b868ff7ecfb8e7ba0d7cdf
Arthur Morais <artgabrielfdm@gmail.com>
2026-05-24 15:40:02 -0300

    Ciclo 2 - Domínio Animal - GREEN

--- projeto-veterinario-springboot-h2-master/gerenciador-Veterinarios/src/main/java/org/iftm/gerenciadorveterinarios/servicies/AnimalService.java
+++ projeto-veterinario-springboot-h2-master/gerenciador-Veterinarios/src/main/java/org/iftm/gerenciadorveterinarios/servicies/AnimalService.java
@@ -1,5 +1,7 @@
 package org.iftm.gerenciadorveterinarios.servicies;
 
+import java.util.List;
+
 import org.iftm.gerenciadorveterinarios.entities.Animal;
 import org.iftm.gerenciadorveterinarios.repositories.AnimalRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,8 +13,15 @@ public class AnimalService {
     @Autowired
     private AnimalRepository repositorio;
 
+    private static final List<String> ESPECIES_ATENDIDAS = List.of("cachorro", "coelho", "gato", "pássaro");
+
     public Animal cadastrar(Animal animal) {
+        if (!ESPECIES_ATENDIDAS.contains(animal.getEspecie())) {
+            throw new IllegalArgumentException("Espécie " + animal.getEspecie() + " não atendida");
+        }
+
         animal.setInternado(true);
+        
         return repositorio.save(animal);
     }
 }
