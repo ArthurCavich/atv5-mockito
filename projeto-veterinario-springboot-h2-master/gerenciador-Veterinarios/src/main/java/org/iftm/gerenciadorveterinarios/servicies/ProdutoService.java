eca1b36cc3800dc4fed528f56b3c9b72b367c96d
Arthur Morais <artgabrielfdm@gmail.com>
2026-05-24 16:05:30 -0300

    Ciclo 1 - Domínio Produto - RED

--- projeto-veterinario-springboot-h2-master/gerenciador-Veterinarios/src/main/java/org/iftm/gerenciadorveterinarios/servicies/ProdutoService.java
+++ projeto-veterinario-springboot-h2-master/gerenciador-Veterinarios/src/main/java/org/iftm/gerenciadorveterinarios/servicies/ProdutoService.java
@@ -1,5 +1,6 @@
 package org.iftm.gerenciadorveterinarios.servicies;
 
+import org.iftm.gerenciadorveterinarios.entities.Produto;
 import org.iftm.gerenciadorveterinarios.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,4 +10,9 @@ public class ProdutoService {
 
     @Autowired
     private ProdutoRepository repositorio;
+
+    public Produto cadastrar(Produto produto) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'cadastrar'");
+    }
 }
--- projeto-veterinario-springboot-h2-master/gerenciador-Veterinarios/src/test/java/org/iftm/gerenciadorveterinarios/servicies/ProdutoServiceTest.java
+++ projeto-veterinario-springboot-h2-master/gerenciador-Veterinarios/src/test/java/org/iftm/gerenciadorveterinarios/servicies/ProdutoServiceTest.java
@@ -1,6 +1,16 @@
 package org.iftm.gerenciadorveterinarios.servicies;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.math.BigDecimal;
+
+import org.iftm.gerenciadorveterinarios.entities.Produto;
 import org.iftm.gerenciadorveterinarios.repositories.ProdutoRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -14,4 +24,18 @@ public class ProdutoServiceTest {
 
     @InjectMocks
     private ProdutoService service;
+
+    @Test
+    @DisplayName("Deve cadastrar produto com status")
+    public void deveCadastrarAnimalComStatusAtivo(){
+        Produto produto = new Produto(1, "Livro de ameaças de RPG", BigDecimal.valueOf(246), 32, false);
+
+        when(repositorio.save(any(Produto.class))).thenReturn(produto);
+
+        Produto salvo = service.cadastrar(produto);
+
+        assertTrue(salvo.isAtivo());
+        verify(repositorio).save(any(Produto.class));
+    }
+
 }
