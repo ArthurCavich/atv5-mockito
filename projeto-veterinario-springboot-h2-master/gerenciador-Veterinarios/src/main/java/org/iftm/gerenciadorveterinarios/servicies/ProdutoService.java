d9e4d22770deaf53be1189e7393594ba0f0ed4e6
Arthur Morais <artgabrielfdm@gmail.com>
2026-05-24 16:28:25 -0300

    Ciclo 2 - Domínio Produto - GREEN

--- projeto-veterinario-springboot-h2-master/gerenciador-Veterinarios/src/main/java/org/iftm/gerenciadorveterinarios/servicies/ProdutoService.java
+++ projeto-veterinario-springboot-h2-master/gerenciador-Veterinarios/src/main/java/org/iftm/gerenciadorveterinarios/servicies/ProdutoService.java
@@ -1,5 +1,7 @@
 package org.iftm.gerenciadorveterinarios.servicies;
 
+import java.math.BigDecimal;
+
 import org.iftm.gerenciadorveterinarios.entities.Produto;
 import org.iftm.gerenciadorveterinarios.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +14,10 @@ public class ProdutoService {
     private ProdutoRepository repositorio;
 
     public Produto cadastrar(Produto produto) {
+        if ((produto.getPreco().compareTo(BigDecimal.valueOf(0))) < 0) {
+            throw new IllegalArgumentException("O preço " + produto.getPreco() + " é inválido para o produto");
+        }
+
         produto.setAtivo(true);
 
         return repositorio.save(produto);
